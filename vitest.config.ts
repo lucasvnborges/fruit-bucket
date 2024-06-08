02ef3356b110974ff718de7c927a4a0b0fab6a79
Lucas Borges <lucasvnborges@gmail.com>
2024-06-08 18:41:15 -0300

    setup: configuração para utilizar a biblioteca vitest

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { mergeConfig, defineConfig, configDefaults } from "vitest/config";
+import viteConfig from "./vite.config";
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      globals: true,
+      environment: "jsdom",
+      coverage: {
+        include: ["src/**"],
+      },
+      exclude: [...configDefaults.exclude],
+    },
+  })
+);
