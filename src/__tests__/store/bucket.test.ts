6eee3d93e786a434f0836690df52ebc555b4c4f0
Lucas Borges <lucasvnborges@gmail.com>
2024-06-08 20:12:05 -0300

    feat: criar testes para os cálculos de ocupação do balde e preço total das frutas

--- src/__tests__/store/bucket.test.ts
+++ src/__tests__/store/bucket.test.ts
@@ -89,7 +89,8 @@ describe("Gerenciamento de estado dos baldes", () => {
     const { addBucket, addFruitToBucket } = result.current;
 
     act(() => addBucket(bucket_mock));
-    for (let i = 1; i <= bucket_mock.fruitCapacity + 1; i++) {
+
+    for (let i = 0; i <= bucket_mock.fruitCapacity; i++) {
       act(() =>
         addFruitToBucket(bucket_mock.id, { ...fruit_mock, id: `${i}` })
       );
@@ -97,6 +98,48 @@ describe("Gerenciamento de estado dos baldes", () => {
 
     const bucket = result.current.buckets.find((b) => b.id === bucket_mock.id);
     expect(bucket?.fruits.length).toBe(bucket_mock.fruitCapacity);
+  });
+
+  test("Espera calcular corretamente o preenchimento do balde", () => {
+    const { result } = renderHook(() => useBucketStore());
+    const { addBucket, addFruitToBucket } = result.current;
+
+    act(() => addBucket(bucket_mock));
+
+    const bucket = () =>
+      result.current.buckets.find((b) => b.id === bucket_mock.id);
+
+    const fruits = [
+      fruit_mock,
+      { ...fruit_mock, id: "2" },
+      { ...fruit_mock, id: "3" },
+    ];
+    const occupations = [33.3, 66.7, 100];
+
+    fruits.forEach((fruit, index) => {
+      act(() => addFruitToBucket(bucket_mock.id, fruit));
+      expect(bucket()?.occupation).toBe(occupations[index]);
+    });
+  });
+
+  test("Espera calcular corretamente o preço total das frutas", () => {
+    const { result } = renderHook(() => useBucketStore());
+    const { addBucket, addFruitToBucket } = result.current;
+
+    act(() => addBucket(bucket_mock));
+
+    const fruits = [
+      fruit_mock,
+      { ...fruit_mock, id: "2" },
+      { ...fruit_mock, id: "3" },
+    ];
+
+    fruits.forEach((fruit) => {
+      act(() => addFruitToBucket(bucket_mock.id, fruit));
+    });
+
+    const bucket = result.current.buckets.find((b) => b.id === bucket_mock.id);
+    expect(bucket?.totalPrice).toBe(13.5);
   });
 
   test("Espera remover uma fruta da lista", () => {
