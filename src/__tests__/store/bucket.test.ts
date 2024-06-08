78b54967869ee07fbd40cd7fb4d1dc7d8d2bfb0d
Lucas Borges <lucasvnborges@gmail.com>
2024-06-08 19:07:17 -0300

    feat: melhoria na organização de adição dos testes de adicionar multiplas frutas e remover fruta

--- src/__tests__/store/bucket.test.ts
+++ src/__tests__/store/bucket.test.ts
@@ -27,7 +27,7 @@ describe("Gerenciamento de estado dos buckets", () => {
     expect(result.current.buckets.length).toEqual(0);
   });
 
-  test("Espera adicionar 1 bucket na lista", () => {
+  test("Espera adicionar um balde na lista", () => {
     const { result } = renderHook(() => useBucketStore());
     const { addBucket } = result.current;
 
@@ -35,7 +35,7 @@ describe("Gerenciamento de estado dos buckets", () => {
     expect(result.current.buckets.length).toEqual(1);
   });
 
-  test("Espera remover 1 bucket da lista", () => {
+  test("Espera remover um balde da lista", () => {
     const { result } = renderHook(() => useBucketStore());
     const { addBucket, deleteBucket } = result.current;
 
@@ -56,10 +56,45 @@ describe("Gerenciamento de estado dos buckets", () => {
     act(() => addFruitToBucket(bucket_mock.id, fruit_mock));
 
     const bucket = result.current.buckets.find((b) => b.id === bucket_mock.id);
-    const fruit = bucket?.fruits.find(f => f.id === fruit_mock.id)
-
     expect(bucket).toBeTruthy();
-    expect(bucket).toBeTypeOf('object');
+    expect(bucket).toBeTypeOf("object");
+    
+    const fruit = bucket?.fruits.find((f) => f.id === fruit_mock.id);
     expect(fruit).toBeTruthy();
+  });
+
+  test("Espera adicionar frutas até o limite do balde", () => {
+    const { result } = renderHook(() => useBucketStore());
+    const { addBucket, addFruitToBucket } = result.current;
+
+    act(() => addBucket(bucket_mock));
+    for (let i = 1; i <= bucket_mock.fruitCapacity + 1; i++) {
+      act(() =>
+        addFruitToBucket(bucket_mock.id, { ...fruit_mock, id: `${i}` })
+      );
+    }
+
+    const bucket = result.current.buckets.find((b) => b.id === bucket_mock.id);
+
+    expect(bucket?.fruits.length).toBe(bucket_mock.fruitCapacity);
+  });
+
+  test("Espera remover uma fruta da lista", () => {
+    const { result } = renderHook(() => useBucketStore());
+    const { addBucket, addFruitToBucket, removeFruitFromBucket } =
+      result.current;
+
+    const bucket = () =>
+      result.current.buckets.find((b) => b.id === bucket_mock.id);
+    const fruit = () => bucket()?.fruits.find((f) => f.id === fruit_mock.id);
+
+    act(() => addBucket(bucket_mock));
+    act(() => addFruitToBucket(bucket_mock.id, fruit_mock));
+
+    expect(bucket()).toBeTruthy();
+    expect(bucket()).toBeTypeOf("object");
+    expect(fruit()).toBeTruthy();
+    act(() => removeFruitFromBucket(bucket_mock.id, fruit_mock.id));
+    expect(fruit()).toBeFalsy();
   });
 });
