17140d9a5a715ab917fb235dd43fc50750b29ecc
Lucas Borges <lucasvnborges@gmail.com>
2024-06-09 00:47:03 -0300

    feat: criar modal para adicionar buckets

---
+++ src/components/CreateBucketModal.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { Form, InputNumber, Modal } from "antd";
+import useBucketStore from "../store/bucket.store";
+
+type Props = {
+  visibility: boolean;
+  toggleVisibility: () => void;
+};
+
+export default function CreateBucketModal({
+  visibility,
+  toggleVisibility,
+}: Props) {
+  const { addBucket } = useBucketStore();
+
+  const [form] = Form.useForm();
+  const [isBusy, setIsBusy] = useState(false);
+
+  function createBucket() {
+    const id = Date.now().toString();
+    const fruitCapacity = form.getFieldValue("fruitCapacity");
+
+    const bucket = {
+      id,
+      fruits: [],
+      occupation: 0,
+      totalPrice: 0,
+      fruitCapacity,
+    };
+
+    setIsBusy(true);
+    addBucket(bucket);
+    handleCloseModal();
+  }
+
+  function handleCloseModal() {
+    setIsBusy(false);
+    form?.resetFields();
+    toggleVisibility();
+  }
+
+  return (
+    <Modal
+      open={visibility}
+      okText="Confirmar"
+      cancelText="Cancelar"
+      title="Criar novo balde"
+      onOk={createBucket}
+      onCancel={handleCloseModal}
+      okButtonProps={{ loading: isBusy }}
+    >
+      <p>Informe abaixo o número limite de frutas possíveis nesse balde</p>
+      <Form form={form} layout="vertical">
+        <Form.Item
+          name="fruitCapacity"
+          rules={[
+            {
+              required: true,
+              message: "Por favor insira um número!",
+            },
+            {
+              type: "number",
+              min: 1,
+              message: "O número limite deve ser maior que zero!",
+            },
+          ]}
+        >
+          <InputNumber type="number" />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+}
