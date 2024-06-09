81b169376a2ad937278223eb514b2179593ecc1b
Lucas Borges <lucasvnborges@gmail.com>
2024-06-09 14:51:54 -0300

    refactor: melhorar escrita do hook de dnd, add listagem de frutas e indicador de capacidade total do balde

--- src/components/Bucket.tsx
+++ src/components/Bucket.tsx
@@ -1,6 +1,6 @@
 import type { CSSProperties, FC } from "react";
 import { useDrop } from "react-dnd";
-import { Button, Col, Row } from "antd";
+import { Button, Card, Col, Flex, Progress, Row } from "antd";
 import useBucketStore from "../store/bucket.store";
 
 export interface Props {
@@ -8,19 +8,12 @@ export interface Props {
 }
 
 export const Bucket: FC<Props> = ({ bucket }) => {
-  const { deleteBucket } = useBucketStore();
+  const { deleteBucket, removeFruitFromBucket } = useBucketStore();
 
-  const [, drop] = useDrop(
-    () => ({
-      accept: "box",
-      drop: () => ({}),
-      collect: (monitor) => ({
-        isOver: monitor.isOver(),
-        canDrop: monitor.canDrop(),
-      }),
-    }),
-    []
-  );
+  const [, drop] = useDrop(() => ({
+    accept: "box",
+    drop: () => ({ id: bucket.id }),
+  }));
 
   return (
     <Col xs={24} md={12}>
@@ -36,13 +29,35 @@ export const Bucket: FC<Props> = ({ bucket }) => {
           </Button>
         </Row>
         <Row style={{ ...header }}>
-          <p>
+          <p style={{ ...rightspace }}>
             Total: <b>R$ {bucket.totalPrice}</b>
           </p>
-          <p style={{ marginLeft: 6 }}>
+          <p>
             {bucket.fruits.length}/{bucket.fruitCapacity}
           </p>
         </Row>
+
+        <Flex wrap gap={4}>
+          {bucket.fruits.map((fruit, index) => (
+            <Card size="small" key={fruit.id + index}>
+              <span style={{ ...rightspace }}>{fruit.name}</span>
+
+              <Button
+                size="small"
+                type="dashed"
+                onClick={() => removeFruitFromBucket(bucket.id, fruit.id)}
+              >
+                x
+              </Button>
+            </Card>
+          ))}
+        </Flex>
+
+        <Progress
+          style={{ ...progress }}
+          percent={bucket.occupation}
+          steps={bucket.fruitCapacity}
+        />
       </div>
     </Col>
   );
@@ -63,4 +78,12 @@ const container: CSSProperties = {
 const header: CSSProperties = {
   display: "flex",
   justifyContent: "space-between",
+};
+
+const rightspace: CSSProperties = {
+  marginRight: 6,
+};
+
+const progress: CSSProperties = {
+  marginTop: 6,
 };
