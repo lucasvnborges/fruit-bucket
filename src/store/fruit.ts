3e493f8f19c5682f2ecec0ecd845823df0cbafc4
Lucas Borges <lucasvnborges@gmail.com>
2024-06-13 17:36:13 -0300

    Update fruit.ts

--- src/store/fruit.ts
+++ src/store/fruit.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { createJSONStorage, persist } from "zustand/middleware";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 type FruitStore = {
   fruits: Fruit[];
